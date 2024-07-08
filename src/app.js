6d5619555917e6ec192a8aba56fadd22af666b26
Iván Fuentes Mahíllo <169067125+IFM1979@users.noreply.github.com>
2024-07-08 11:03:19 +0000

    Generador de Excusas

--- src/app.js
+++ src/app.js
@@ -4,8 +4,33 @@ import "./style.css";
 
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
+function generateExcuse() {
+  const who = ["El perro", "Mi abuela", "El cartero", "Mi pájaro"];
+  const action = ["se comió", "se hizo pis", "estrelló", "rompió"];
+  const what = ["mis deberes", "mi teléfono", "el coche"];
+  const when = [
+    "antes de la clase",
+    "mientras dormía",
+    "mientras hacía ejercicio",
+    "durante mi comida",
+    "mientras rezaba"
+  ];
 
+  const whoIndex = Math.floor(Math.random() * who.length);
+  const actionIndex = Math.floor(Math.random() * action.length);
+  const whatIndex = Math.floor(Math.random() * what.length);
+  const whenIndex = Math.floor(Math.random() * when.length);
+
+  return (
+    who[whoIndex] +
+    " " +
+    action[actionIndex] +
+    " " +
+    what[whatIndex] +
+    " " +
+    when[whenIndex]
+  );
+}
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  document.getElementById("excuse").innerHTML = generateExcuse();
 };
